5fa89f4156e0ddf6b66920c7cbc1b3ea9d15ae7e
tychoengberink <464544@student.saxion.nl>
2019-11-20 13:25:55 +0100

    Finished the assignment

--- main.cpp
+++ main.cpp
@@ -6,31 +6,50 @@
 using namespace std;
 
 int randomNumber();
-void resetGame(char *choice);
+
+void resetGame();
+
+void game();
 
 int tries = 0, guess, random;
-char *reset;
+char reset = ' ';
 
 
 int main() {
+    game();
+    return 0;
+}
+
+void game() {
     random = randomNumber();
     cout << "Lets play a game. I'm thinking of a number." << endl;
     cout << "You have 7 tries to guess what it is" << endl;
-    while (tries < 7) {
+    while (tries != 7) {
         guess = readNumber(tries);
         if (writeResponse(guess, random)) {
             tries = 7; //stop because we won
             cout << "Correct! You Win!" << endl;
-            cout << "Would  you like to play again (y/n)?";
-            cin >> reset;
-            resetGame(reset);
         } else {
             tries++;
         }
     }
-    return 0;
+    if (tries == 7) {
+        cout << "Sorry, you lose.  The correct number was " + to_string(random) + "." << endl;
+    }
+    resetGame();
 }
 
+void resetGame() {
+    cout << "Would  you like to play again (y/n)?";
+    cin >> reset;
+    if (reset == 'y') {
+        tries = 0;
+        reset = ' ';
+        game();
+    } else if (reset == 'n') {
+        exit(EXIT_SUCCESS);
+    }
+}
 
 // Initialize our mersenne twister with a random seed based on the clock
 std::mt19937 mersenne(static_cast<unsigned int>(time(nullptr)));
@@ -39,13 +58,4 @@ std::uniform_int_distribution<> generator(1, 100);
 
 int randomNumber() {
     return generator(mersenne);
-}
-
-void resetGame(char *choice){
-    if(*choice == *"y"){
-        tries = 0;
-    }else if(*choice == *"n"){
-        exit(0);
-    }
-
 }
